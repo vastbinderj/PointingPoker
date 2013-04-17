b4fac574369134083eff7807954b234a24c71857
James W Vastbinder Jr <james@thorin.local>
2013-04-17 10:04:49 -0700

    Added build.sbt and switched to Slick

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,10 +8,12 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
+      "com.typesafe.slick" %% "slick" % "1.0.0",
+      "org.slf4j" % "slf4j-simple" % "1.6.4"
       // Add your project dependencies here,
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
